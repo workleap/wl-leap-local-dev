83ff81a633a9393ae717303cfb8363ed384a4301
Guillaume Caya-Letourneau <guillaume.caya@gsoft.com>
2024-03-04 20:51:41 +0000

    Merged PR 5193: IDP-899 - Change eventGrid env var name
    Changed eventgrid env var name

--- src/Leap.Cli/Dependencies/EventGridDependencyHandler.cs
+++ src/Leap.Cli/Dependencies/EventGridDependencyHandler.cs
@@ -4,6 +4,7 @@ using Leap.Cli.Model;
 using Microsoft.Extensions.Logging;
 using System.IO.Abstractions;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace Leap.Cli.Dependencies;
 
@@ -12,6 +13,7 @@ internal sealed class EventGridDependencyHandler : DependencyHandler<EventGridDe
     private const int EventGridPort = 6500;
     private readonly IConfigureDockerCompose _dockerCompose;
     private readonly IConfigureEnvironmentVariables _environmentVariables;
+    private readonly IConfigureAppSettingsJson _appSettingsJson;
     private readonly IFileSystem _fileSystem;
     private readonly ILogger<EventGridDependencyHandler> _logger;
 
@@ -24,11 +26,13 @@ internal sealed class EventGridDependencyHandler : DependencyHandler<EventGridDe
     public EventGridDependencyHandler(
         IConfigureDockerCompose dockerCompose,
         IConfigureEnvironmentVariables environmentVariables,
+        IConfigureAppSettingsJson appSettingsJson,
         IFileSystem fileSystem,
         ILogger<EventGridDependencyHandler> logger)
     {
         this._dockerCompose = dockerCompose;
         this._environmentVariables = environmentVariables;
+        this._appSettingsJson = appSettingsJson;
         this._fileSystem = fileSystem;
         this._logger = logger;
     }
@@ -38,6 +42,7 @@ internal sealed class EventGridDependencyHandler : DependencyHandler<EventGridDe
         await this.EnsureEventGridSettingsFileExists(cancellationToken);
         ConfigureDockerCompose(this._dockerCompose.Configuration);
         this._environmentVariables.Configure(ConfigureEnvironmentVariables);
+        ConfigureAppSettingsJson(this._appSettingsJson.Configuration);
     }
 
     protected override Task AfterStartAsync(EventGridDependency dependency, CancellationToken cancellationToken)
@@ -67,9 +72,14 @@ internal sealed class EventGridDependencyHandler : DependencyHandler<EventGridDe
     {
         environmentVariables.AddRange(new[]
         {
-            new EnvironmentVariable("AZURE__EVENTGRID__URI", EventGridHostUrl, EnvironmentVariableScope.Host),
-            new EnvironmentVariable("AZURE__EVENTGRID__URI", EventGridContainerUrl, EnvironmentVariableScope.Container),
+            new EnvironmentVariable("AZURE__EVENTGRID__ENDPOINT", EventGridHostUrl, EnvironmentVariableScope.Host),
+            new EnvironmentVariable("AZURE__EVENTGRID__ENDPOINT", EventGridContainerUrl, EnvironmentVariableScope.Container),
         });
+    }
+
+    private static void ConfigureAppSettingsJson(JsonObject appsettings)
+    {
+        appsettings["Azure:EventGrid:Endpoint"] = EventGridHostUrl;
     }
 
     private async Task EnsureEventGridSettingsFileExists(CancellationToken cancellationToken)
